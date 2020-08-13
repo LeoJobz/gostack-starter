3b30708f7b5e4c101d9b516dd146abab84ca2523
Leonardo Santos <leonardo.cienciacomputer@gmail.com>
2020-08-13 15:40:13 -0300

    es6_classes

--- 02es6/main.js
+++ 02es6/main.js
@@ -1,11 +1,38 @@
-alert('teste')
-
-class Teste {
-    metodo() {
-
+class List {
+    constructor() {
+        this.data = []
     }
 
-    outro() {
-        
+    add(data) {
+        this.data.push(data)
+        console.log(this.data)
     }
 }
+
+class TodoList extends List {
+    constructor() {
+        super();
+        this.nome = 'Leo'
+    }
+
+    mostraNome() {
+        console.log(this.nome)
+    }
+}
+
+var minhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function () {
+    minhaLista.add('novotodo')
+}
+
+minhaLista.mostraNome();
+
+//Static methods
+class Matematica {
+    static soma(a, b) {
+        return a + b
+    }
+}
+
+console.log(Matematica.soma(1,2))
