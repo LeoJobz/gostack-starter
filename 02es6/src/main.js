bd5a7961d7132d6c3dbc480a6e22f05bd2aa1f3a
Leonardo Santos <leonardo.cienciacomputer@gmail.com>
2020-08-13 23:06:56 -0300

    es6_Async/await

--- 02es6/src/main.js
+++ 02es6/src/main.js
@@ -170,4 +170,19 @@
 // console.log(usuario2)
 //#endregion
 
-alert('testando')
+const minhaPromise = () => new Promise((resolve, reject) => {
+    setTimeout(() => { resolve('OK') }, 2000)
+}) 
+
+// async function executaPromise() {
+//     const response = await minhaPromise();
+
+//     console.log(response)
+// }
+
+const executaPromise = async () => {
+    console.log(await minhaPromise())
+}
+
+
+executaPromise()
